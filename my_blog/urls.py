bcc696387ad2c958d310e7bbb20922eb91d2194a
631643983 <631643983@qq.com>
2018-02-24 21:17:26 +0800

    Fralychen study python blog

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 from article import views
+
 urlpatterns = [
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', admin.site.urls),
-	url(r'^$', views.home),
-    ]
+    url(r'^$', views.home, name = 'home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+]
